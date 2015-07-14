8004d4508058302a4c7db119701d26fcdfa157c4
cyrusChiu <ppk28.tw@gmail.com>
2015-07-14 11:03:14 +0800

    modified

--- download_spam_data.sh
+++ download_spam_data.sh
@@ -1,8 +1,8 @@
-if [ ! -f "./spam.zip" ]&&[ ! -f "./spam"]; then 
+if [ ! -f "./spam.zip" ]&&[ ! -f "./spam" ]; then 
    wget https://s3-ap-northeast-1.amazonaws.com/pixnet-hackathon-2015/spam/spam.zip
    unzip spam.zip
 fi
-if [ ! -f "./spam"]&&[ -f "./spam.zip"]; then
+if [ ! -f "./spam" ]&&[ -f "./spam.zip" ]; then
    unzip spam.zip
 fi
 
